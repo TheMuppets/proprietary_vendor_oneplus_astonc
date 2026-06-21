5910f6cc654879c8f8a31671355e3825130d02a5
Bruno Martins <bgcngm@gmail.com>
2026-06-21 11:02:45 +0530

    astonc: De-blob oplus interfaces
    Change-Id: If38ae1a7d030c837cdbf516de6f5930b60573ac6

--- astonc-vendor.mk
+++ astonc-vendor.mk
@@ -1145,9 +1145,6 @@ PRODUCT_PACKAGES += \
     libthreadutils \
     libubifocus \
     libvideoml \
-    vendor.oplus.hardware.cameraextension-V1-ndk \
-    vendor.oplus.hardware.cammidasservice-V1-ndk_vendor \
-    vendor.oplus.hardware.commondcs-V1-ndk_platform \
     vendor.qti.hardware.camera.aon-service-impl \
     vendor.qti.hardware.camera.postproc@1.0-service-impl \
     com.qti.sensor.imx355 \
@@ -1344,12 +1341,9 @@ PRODUCT_PACKAGES += \
     libyuv2 \
     libyuvwrapper \
     libziparchive_odm \
-    vendor.oplus.hardware.camera_rfi-V1-ndk \
     vendor.oplus.hardware.camera_rfi-V1-service-impl \
     vendor.oplus.hardware.cameraextension-V1-service-impl \
-    vendor.oplus.hardware.cammidasservice-V1-ndk \
     vendor.oplus.hardware.sendextcamcmd-V1-service-impl \
-    vendor.oplus.hardware.sendextcamcmd-V2-ndk \
     odm_lib_rfsa_adsp_libtfadsp_sb4_0_rx_so \
     odm_lib_rfsa_adsp_libtfadsp_sb4_0_tx_so \
     vendor.qti.camera.aon-impl-1.3.xml \
